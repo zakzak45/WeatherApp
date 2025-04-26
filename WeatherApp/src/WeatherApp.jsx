6418b1e84fa49b6b03ff1deb16a8090c34642a90
zakzak45 <147825634+zakzak45@users.noreply.github.com>
2025-04-26 12:37:24 +0200

    added an api

--- WeatherApp/src/WeatherApp.jsx
+++ WeatherApp/src/WeatherApp.jsx
@@ -4,11 +4,11 @@ import { useEffect, useState } from 'react'
 function Weather() {
 const [weather ,setWeather]=useState(null);
 
-const URL ="";
+const URL ="https://afrigis.services/weather-10-day-forecast/v1/getHourlyByCoords?latitude=2&longitude=2&station_count=1&location_buffer=1&day_count=1";
 
 const WeatherApp = async() => {
  try{
-  const response = await Fetch();
+  const response = await Fetch(URL);
   const data = await response.json();
   setWeather(data);
  }catch(erro){
@@ -27,7 +27,14 @@ useEffect(()=>{
 
   return (
     <>
-    
+    <div>
+    <h1>Weather App</h1>
+    <h2>{Weather.name}</h2>
+    <p>{Weather.Weather[0].feels_like}</p>
+    <p>Temperature: {Weather.main.t}</p>
+    <p>Feels Like: {Weather.main.feels_like}</p>
+    <p>Humidity: {Weather.main.humidity}</p>
+      </div>
     </>
   )
 }
